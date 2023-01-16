94ba54865c5c152de7b09803d9da21f644597543
KartikECE <117670174+KartikECE@users.noreply.github.com>
2023-01-16 20:31:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 //your code here
-let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
+let input = document.getElementById("input")
+let expression="";
+function solve(e){
+  if(e == '='){
+    let result = eval(expression); // eval is inbuilt function which calculate/evaluate whatever the expression it will give the answer
+    // console.log(result); we check the output in console by console log
+    input.value = result;
+  }
+
+  else if(e == "C"){
+    expression = "";
+    input.value ="";
+  }
+  else{
+    expression += e;
+    // console.log(expression);
+    input.value = expression;
+  }
+}
     else if(e.target.innerHTML == 'C'){
       string = ""
       document.querySelector('input').value = string;
